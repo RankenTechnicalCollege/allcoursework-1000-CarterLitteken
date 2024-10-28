03e7cfb311b526420bb4b54a0fad04f594f7ff4a
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-10-28 10:11:18 -0500

    ch3lab1

--- labs/JS_CH03_LAB/P1/future_value.js
+++ labs/JS_CH03_LAB/P1/future_value.js
@@ -2,31 +2,43 @@ let again = "y"
 
 do {
   let investment = 0;
+
   do {
-    investment = parseFloat(prompt("Enter investment amount as xxxxx.xx", 10000));
+    investment = parseFloat(prompt("Enter investment amount as xxxxx.xx\n"
+      + "(Must be valid positive number)", 10000));
   }
-  while ( isNaN(investment) && 0 < investment );
+  while ( isNaN(investment) || investment <= 0);
 
   let rate = 0;
   do {
-    rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
+    rate = parseFloat(prompt("Enter interest rate as xx.x\n"
+      + "(Must be a number above 0 to 15)", 7.5));
   }
-  while ( isNaN(rate) && 0 < rate );
+  while ( isNaN(rate) || 0 <= rate < 15);
 
   let years = 0;
   do {
-    years = parseInt(prompt("Enter number of years", 10));
+    years = parseInt(prompt("Enter number of years\n"
+      + "(Must be valid positive number)", 10));
   }
-  while ( isNaN(years) && 0 < years );
-
-  let futureValue = investment;
-  for (let i = 1; i <= years; i++ ) {
-    futureValue += futureValue * rate / 100;
-  }
+  while ( isNaN(years) || years <= 0);
 
   document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
   document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
   document.write(`<p><label>Years:</label> ${years}</p>`);
-  document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`);
+
+  let futureValue = investment;
+  let interest = 0;
+  for (let i = 1; i <= years; i++ ) {
+    interest = futureValue * rate / 100;
+    document.write(`<p><label>Year:</label> ${i}</p>`);
+    document.write(`<p><label>Interest:</label> ${interest}</p>`);
+    futureValue += interest
+    document.write(`<p><label>Value:</label> ${futureValue.toFixed(2)}</p>`);
+
+  }
+
+  again = prompt("Repeat entries? (y/n)", "y")
+
 }
 while (again == "y");
