00117e25921414468d33de1a0de4b00a5e821bd7
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-10-30 08:16:09 -0500

    hot

---
+++ tests/JS_HOT1/favorite-movie-list.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const favoriteMovies = [];
+favoriteMovies[0] = "The Iron Giant";
+favoriteMovies[1] = "The Fast and the Furious: Tokyo Drift";
+favoriteMovies[2] = "Speed Racer";
+
