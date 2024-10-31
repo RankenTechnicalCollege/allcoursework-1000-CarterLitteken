6655c9339b5281d883d56e856c283fa97b1e1811
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-10-31 11:20:17 -0500

    ch4lab

---
+++ labs/JS_CH04_LAB/mpg.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const getErrorMsg = lbl =>
+  `${lbl} must a valid number greater than zero.`;
+
+const focusAndSelect = selector => {
+  const elem = $(selector);
+  elem.focus();
+  elem.select();
+};
+
+const processEntries = () => {
+  const miles = parseFloat($("#miles").value);
+  const gallons = parseFloat($("#gallons").value);
+
+  if (isNaN(miles) || miles <= 0) {
+    alert(getErrorMsg("Miles driven"));
+    focusAndSelect("#miles");
+  } else if (isNaN(gallons) || gallons <= 0) {
+    alert(getErrorMsg("Gallons of gas used"));
+    focusAndSelect("#gallons");
+  } else {
+    $("#mpg").value = (miles / gallongs).toFixed(2);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  $("#calculate").addEventListener("click", processEntries);
+  $("#miles").focus();
+});
