ba3fa582411c2f18dad5a4d94bfd5bc3cd446d8f
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-01 08:48:20 -0500

    ch4lab

--- labs/JS_CH04_LAB/future_value.js
+++ labs/JS_CH04_LAB/future_value.js
@@ -4,3 +4,25 @@ do {
     prompt("Enter investment amount as xxxx.xx", 10000));
 }
 while ( isNaN(investment) );
+
+let rate = 0;
+do {
+  rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
+}
+while ( isNaN(rate) );
+
+let years = 0;
+do {
+  years = parseFloat(prompt("Enter number of years", 10));
+}
+while ( isNaN(years) );
+
+let futureValue = investment;
+for (let i = 1; i <= years; i++ ) {
+  futureValue += futureValue * rate / 100;
+}
+
+document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
+document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
+document.write(`<p><label>Years:</label> ${years}</p>`);
+document.write(`<p><label>Future Value:</label> ${futureValue}</p>`);
