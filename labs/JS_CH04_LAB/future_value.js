030bd0e6cf52917e2b934c19b52b6b7134e1bae3
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-01 10:01:14 -0500

    ch4lab

--- labs/JS_CH04_LAB/future_value.js
+++ labs/JS_CH04_LAB/future_value.js
@@ -7,6 +7,7 @@ const calculateFV() = (investment, rate, years) => {
   for (let i = 1; i <= years; i++) {
     futureValue += futureValue * rate / 100;
   }
+  return futureValue.toFixed(2)
 }
   
 
