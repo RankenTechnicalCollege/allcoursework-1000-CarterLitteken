9b78591ac36a1d69bb7b45fddc1f2d962076b0f3
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-01 10:31:46 -0500

    ch3lab1

---
+++ labs/JS_CH03_LAB/P1/future_value_plus.js
@@ -0,0 +1,43 @@
+let again = "y"
+
+do {
+  let investment = 0;
+
+  do {
+    investment = parseFloat(prompt("Enter investment amount as xxxxx.xx\n"
+      + "(Must be valid positive number)", 10000));
+  }
+  while ( isNaN(investment) || investment <= 0);
+
+  let rate = 0;
+  do {
+    rate = parseFloat(prompt("Enter interest rate as xx.x\n"
+      + "(Must be a number above 0 to 15)", 7.5));
+  }
+  while ( isNaN(rate) || 0 <= rate > 15);
+
+  let years = 0;
+  do {
+    years = parseInt(prompt("Enter number of years\n"
+      + "(Must be valid positive number)", 10));
+  }
+  while ( isNaN(years) || years <= 0);
+
+  document.write(`<h4><label>Investment amount:</label> ${investment} |` + 
+    ` <label>Interest rate:</label> ${rate} |` +
+    ` <label>Years:</label> ${years}</h4>`);
+
+  let futureValue = investment;
+  let interest = 0;
+  for (let i = 1; i <= years; i++ ) {
+    interest = futureValue * rate / 100;
+    futureValue += interest;
+    document.write(`<p><label>Year:</label> ${i} |` +
+      ` <label>Interest:</label> ${interest.toFixed(2)} |` +
+      ` <label>Value:</label> ${futureValue.toFixed(2)}</p>`);
+  }
+
+  again = prompt("Repeat entries? (y/n)", "y")
+
+}
+while (again == "y");
