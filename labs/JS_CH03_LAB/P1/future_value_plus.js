b56d258719971a4341f652316957f57b28a6befe
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-04 11:05:03 -0600

    ch3lab1

--- labs/JS_CH03_LAB/P1/future_value_plus.js
+++ labs/JS_CH03_LAB/P1/future_value_plus.js
@@ -1,43 +1,31 @@
-let again = "y"
+let investment = 0;
 
 do {
-  let investment = 0;
-
-  do {
-    investment = parseFloat(prompt("Enter investment amount as xxxxx.xx\n"
-      + "(Must be valid positive number)", 10000));
-  }
-  while ( isNaN(investment) || investment <= 0);
-
-  let rate = 0;
-  do {
-    rate = parseFloat(prompt("Enter interest rate as xx.x\n"
-      + "(Must be a number above 0 to 15)", 7.5));
-  }
-  while ( isNaN(rate) || 0 <= rate > 15);
-
-  let years = 0;
-  do {
-    years = parseInt(prompt("Enter number of years\n"
-      + "(Must be valid positive number)", 10));
-  }
-  while ( isNaN(years) || years <= 0);
-
-  document.write(`<h4><label>Investment amount:</label> ${investment} |` + 
-    ` <label>Interest rate:</label> ${rate} |` +
-    ` <label>Years:</label> ${years}</h4>`);
-
-  let futureValue = investment;
-  let interest = 0;
-  for (let i = 1; i <= years; i++ ) {
-    interest = futureValue * rate / 100;
-    futureValue += interest;
-    document.write(`<p><label>Year:</label> ${i} |` +
-      ` <label>Interest:</label> ${interest.toFixed(2)} |` +
-      ` <label>Value:</label> ${futureValue.toFixed(2)}</p>`);
-  } 
-
-  again = prompt("Repeat entries? (y/n)", "y")
-
+  investment = parseFloat(prompt("Enter investment amount as xxxxx.xx\n"
+    + "(Must be valid positive number)", 10000));
 }
-while (again == "y");
+while ( isNaN(investment) || investment <= 0);
+
+let rate = 0;
+do { 
+  rate = parseFloat(prompt("Enter interest rate as xx.x\n"
+    + "(Must be a number above 0 to 15)", 7.5));
+}
+while ( isNaN(rate) || 0 <= rate > 15);
+
+let years = 0;
+do {
+  years = parseInt(prompt("Enter number of years\n"
+    + "(Must be valid positive number)", 10));
+}
+while ( isNaN(years) || years <= 0);
+
+let annualFutureValue = investment;
+let monthlyFutureValue = investment;
+let months = years * 12;
+for (let i = 1; i <= years; i++ ) {
+  annualFutureValue += annualFutureValue * rate / 100;
+}
+for (let i = 1; i <= months; i++ ) {
+  monthlyFutureValue += monthlyFutureValue * (rate / 12) / 100;
+}
