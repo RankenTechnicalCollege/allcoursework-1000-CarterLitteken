d595cedb6a4e56746c78ca97c2bbbbd1fdfbfdec
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-05 09:45:19 -0600

    ch3lab1

--- labs/JS_CH03_LAB/P1/sales_array.js
+++ labs/JS_CH03_LAB/P1/sales_array.js
@@ -7,6 +7,46 @@ const region4 = [1845, 1491, 1284, 1575];
 const region5 = [2120, 1767, 1599, 3888];
 
 let quarterly_sales = 0;
+let quarter = 1;
 let regional_sales = 0;
 let total_sales = 0;
 
+document.write(`<h2>Sales by Quarter</h2>`);
+for (let i in region1) {
+  quarterly_sales = region1[i] + region2[i] + region3[i] + region4[i] + region5[i]
+  document.write(`<p>Q${quarter}: $${quarterly_sales}</p>`);
+  quarter++;
+}
+
+document.write(`<h2>Sales by Region</h2>`);
+for (let val of region1) {
+  regional_sales += val;
+  total_sales += val;
+}
+document.write(`<p>Region 1: $${regional_sales}</p>`);
+regional_sales = 0;
+for (let val of region2) {
+  regional_sales += val;
+  total_sales += val;
+}
+document.write(`<p>Region 2: $${regional_sales}</p>`);
+regional_sales = 0;
+for (let val of region3) {
+  regional_sales += val;
+  total_sales += val;
+}
+document.write(`<p>Region 3: $${regional_sales}</p>`);
+regional_sales = 0;
+for (let val of region4) {
+  regional_sales += val;
+  total_sales += val;
+}
+document.write(`<p>Region 4: $${regional_sales}</p>`);
+regional_sales = 0;
+for (let val of region5) {
+  regional_sales += val;
+  total_sales += val;
+}
+document.write(`<p>Region 5: $${regional_sales}</p>`);
+document.write(`<h2>Total Sales</h2>`)
+document.write(`<p>$${total_sales}</p>`)
