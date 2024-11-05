95908d5be1baa370fad5ded34bc28c7481b7b952
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-05 08:39:52 -0600

    ch3lab1

--- labs/JS_CH03_LAB/P1/letter_grades.js
+++ labs/JS_CH03_LAB/P1/letter_grades.js
@@ -1,2 +1,14 @@
 "use strict";
 
+let grade = 0;
+do {
+  grade = parseFloat(prompt("Enter number grade from 0 to 100\n" + "Or enter 999 to end entries", 999))
+
+  if (grade >= 88 && grade < 100) {
+    document.write(`<p>Grade ${grade} = A`);
+  }
+  
+
+
+}
+while (grade != 999);
