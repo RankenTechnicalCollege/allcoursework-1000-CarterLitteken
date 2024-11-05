9247ae7ee4acb45c7f3d1c9f52f0f91a7a5dc0c0
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-05 11:01:44 -0600

    ch2hw

---
+++ homework/JS_CH02_HW/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+document.getElementById('btnClick').onclick = () => {
+  let burger = parseInt(document.getElementById('burger')) * 6;
+  let fries = parseInt(document.getElementById('fries')) * 4;
+  let soda = parseInt(document.getElementById('soda')) * 3;
+  const total = burger + fries + soda;
+  document.write(`<p>Total: \u0024${total}</p>`)
+};
