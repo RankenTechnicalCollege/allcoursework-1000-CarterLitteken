42cdf5770b86832c7f37462e80bd14161a682209
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-06 11:37:28 -0600

    ch4lab

--- labs/JS_CH04_LAB/future_value.js
+++ labs/JS_CH04_LAB/future_value.js
@@ -15,7 +15,7 @@ const focusAndSelect = selector => {
   elem.focus();
   elem.select();
 };
-
+/*
 const processEntries = () => {
   const investment = parseFloat($("#investment").value);
   const rate = parseFloat($("#rate").value);
@@ -35,53 +35,39 @@ const processEntries = () => {
     focusAndSelect("#years");
   }
   if (error != null) {
-    alert(error.valueOf())
+    alert(error.valueOf());
   }
   else {
     $("#futureValue").value = calculateFV(investment, rate, years).toFixed(2);
   }
 };
-
+*/
 document.addEventListener("DOMContentLoaded", () => {
-  $("#calculate").addEventListener("click", processEntries);
+  $("#calculate").addEventListener("click", function () {
+    const investment = parseFloat($("#investment").value);
+    const rate = parseFloat($("#rate").value);
+    const years = parseFloat($("#years").value);
+    var error = null;
+  
+    if (investment <= 0 || investment > 100000) {
+      error = "Investment must be a number greater than 0 and less then or equal to 100,000.";
+      focusAndSelect("#investment");
+    }
+    else if (rate <= 0 || rate > 15) {
+      error = "Interest rate must be a number greater than 0 and less then or equal to 15.";
+      focusAndSelect("#rate");
+    }
+    else if (years <= 0 || years > 50) {
+      error = "Years must be a number greater than 0 and less then or equal to 50.";
+      focusAndSelect("#years");
+    }
+
+    if (error != null) {
+      alert(error.valueOf());
+    }
+    else {
+      $("#futureValue").value = calculateFV(investment, rate, years).toFixed(2);
+    }
+  }());
   $("#investment").focus();
 });
-
-
-
-
-
-
-
-
-
-/*
-let investment = 0;
-do {
-  investment = parseFloat(
-    prompt("Enter investment amount as xxxx.xx", 10000));
-}
-while ( isNaN(investment) );
-
-let rate = 0;
-do {
-  rate = parseFloat(prompt("Enter interest rate as xx.x", 7.5));
-}
-while ( isNaN(rate) );
-
-let years = 0;
-do {
-  years = parseFloat(prompt("Enter number of years", 10));
-}
-while ( isNaN(years) );
-
-let futureValue = investment;
-for (let i = 1; i <= years; i++ ) {
-  futureValue += futureValue * rate / 100;
-}
-
-document.write(`<p><label>Investment amount:</label> ${investment}</p>`);
-document.write(`<p><label>Interest rate:</label> ${rate}</p>`);
-document.write(`<p><label>Years:</label> ${years}</p>`);
-document.write(`<p><label>Future Value:</label> ${futureValue.toFixed(2)}</p>`);
-*/
