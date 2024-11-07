3dac300b917ea02f0dfb94ce39f39ebe08d5a7d7
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-07 10:45:34 -0600

    ch4lab

---
+++ labs/JS_CH04_LAB/salesTax.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const $ = selector => document.querySelector(selector);
+
+const calculateTax = () => {
+  const subtotal = parseFloat($("#subtotal").value);
+  const taxRate = par
+};
