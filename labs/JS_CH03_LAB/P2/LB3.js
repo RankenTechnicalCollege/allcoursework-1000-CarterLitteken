d2981e69a878a8a67d6f656d5d3174b3b668b1f5
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-13 11:22:25 -0600

    ch3lab2

--- labs/JS_CH03_LAB/P2/LB3.js
+++ labs/JS_CH03_LAB/P2/LB3.js
@@ -1,2 +1,17 @@
 "use strict";
 
+const $ = selector => document.querySelector(selector);
+
+const calcBMI = () => {
+  const weight = parseFloat($("#weight").value);
+  const height = parseFloat($("#height").value);
+
+  const bmi = (weight * 703) / (height * height);
+
+  $("#bmi").value = bmi.toFixed(1);
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  $("#calcBMI").addEventListener("click", calcBMI);
+});
