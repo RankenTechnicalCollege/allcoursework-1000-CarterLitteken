e93a074251f83e949afe53fc4974c41ca65f5bd6
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-13 09:47:57 -0600

    ch3lab2

--- labs/JS_CH03_LAB/LB1.js
+++ labs/JS_CH03_LAB/LB1.js
@@ -3,11 +3,11 @@
 const $ = selector => document.querySelector(selector);
 
 const calculateAvg = () => {
-  const numberOne = parseFloat($("#numberOne").value);
-  const numberTwo = parseFloat($("#numberTwo").value);
-  const numberThree = parseFloat($("#numberThree").value);
-  const numberFour = parseFloat($("#numberFour").value);
-  const numberFive = parseFloat($("#numberFive").value);
+  const number1 = parseFloat($("#number1").value);
+  const number2 = parseFloat($("#number2").value);
+  const number3 = parseFloat($("#number3").value);
+  const number4 = parseFloat($("#number4").value);
+  const number5 = parseFloat($("#number5").value);
 
-  
+  const avg = ()
 };
