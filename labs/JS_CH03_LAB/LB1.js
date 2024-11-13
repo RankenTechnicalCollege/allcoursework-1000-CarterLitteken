45f897b375966fa8013593759f1deea0ba1e6e15
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-13 10:08:02 -0600

    ch3lab2

--- labs/JS_CH03_LAB/LB1.js
+++ labs/JS_CH03_LAB/LB1.js
@@ -9,5 +9,11 @@ const calculateAvg = () => {
   const number4 = parseFloat($("#number4").value);
   const number5 = parseFloat($("#number5").value);
 
-  const avg = ()
+  const avg = (number1 + number2 + number3 + number4 + number5) / 5;
+
+  $("#result").innerHTML = avg.toFixed(2);
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+  $("#calculate").addEventListener("click", calculateAvg);
+});
