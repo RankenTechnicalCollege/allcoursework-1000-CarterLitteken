37bd6ee7a370d343972ec2d2aa5ff79d8f6c934f
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-15 10:56:02 -0600

    ch4lab

--- labs/JS_CH04_LAB/change_calculator.js
+++ labs/JS_CH04_LAB/change_calculator.js
@@ -9,11 +9,38 @@ const focusAndSelect = selector => {
 };
 
 const makeChange = () => {
-  const change = parseInt($("#change").value);
+  let change = parseInt($("#change").value);
+  let quarters = 0;
+  let dimes = 0;
+  let nickles = 0;
+  let pennies = 0;
 
   if (change < 0 || change > 99) {
     alert("Enter number between 0 and 99");
   }
-  
+  else {
+    do {
+      change -= 25;
+      quarters++;
+    }
+    while (change >= 25);
+    do {
+      change -= 10;
+      dimes++;
+    }
+    while (change >= 10);
+    do {
+      change -= 5;
+      nickles++;
+    }
+    while (change >= 5);
+    do {
+      change -= 1;
+      pennies++;
+    }
+    while (change >= 1);
+
+    $("#quarters")
+  }
 
 };
