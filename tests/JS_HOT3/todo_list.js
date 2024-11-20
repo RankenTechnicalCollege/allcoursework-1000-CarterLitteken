d978351bb07c72347b58f0de446ab7c66850a3bd
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-20 11:20:20 -0600

    hot

--- tests/JS_HOT3/todo_list.js
+++ tests/JS_HOT3/todo_list.js
@@ -1,25 +1,27 @@
 "use strict";
 
 const $ = selector => document.querySelector(selector);
-let task_num = 0;
+let task_num = 5;
 const todo = () => {
   const task = $("#task").value;
   const para = document.createElement("li");
   const node = document.createTextNode(task);
   const element = document.getElementById("list");
-  const button = document.createElement("button",  )
+  const button = document.createElement("button",)
   if (task != "") {
     task_num++;
     para.appendChild(node);
     element.appendChild(para);
-    para.setAttribute("class", task_num);
+    para.setAttribute("id", "task" + task_num);
     element.appendChild(button);
-    button.setAttribute("class", task_num);
+    button.setAttribute("id", "btn" + task_num);
+    button.setAttribute("class", "remove m-0 align-self-center text-danger border border-0 bg-white")
   }
+  document.getElementsByClassName("remove m-0 align-self-center text-danger border border-0 bg-white").innerHTML = "x";
 };
 
 const remove = () => {
-  const elements = document.getElementsByClassName("1"); 
+  const elements = document.getElementsByClassName("1");
   elements.remove();
 };
 
