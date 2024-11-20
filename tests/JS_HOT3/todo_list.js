9f11ed7e95d40791682854f2aa981ba806ce3492
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-20 11:44:43 -0600

    hot

--- tests/JS_HOT3/todo_list.js
+++ tests/JS_HOT3/todo_list.js
@@ -1,31 +1,31 @@
 "use strict";
 
 const $ = selector => document.querySelector(selector);
-let task_num = 5;
+let task_num = 6;
 const todo = () => {
   const task = $("#task").value;
   const para = document.createElement("li");
   const node = document.createTextNode(task);
-  const element = document.getElementById("list");
-  const button = document.createElement("button",)
+  const list = $("#list");
+  const button = document.createElement("button");
   if (task != "") {
-    task_num++;
     para.appendChild(node);
-    element.appendChild(para);
+    list.appendChild(para);
     para.setAttribute("id", "task" + task_num);
-    element.appendChild(button);
+    $("#task" + task_num).appendChild(button);
     button.setAttribute("id", "btn" + task_num);
-    button.setAttribute("class", "remove m-0 align-self-center text-danger border border-0 bg-white")
+    button.setAttribute("class", "remove mx-1 align-self-center text-danger border border-0 bg-white")
+    $("#btn" + task_num).innerHTML = "x";
+    task_num++;
   }
-  document.getElementsByClassName("remove m-0 align-self-center text-danger border border-0 bg-white").innerHTML = "x";
+  
 };
 
-const remove = () => {
-  const elements = document.getElementsByClassName("1");
-  elements.remove();
-};
 
 document.addEventListener("DOMContentLoaded", () => {
   $("#add").addEventListener("click", todo);
+  for (i = 0; i < task_num, i++;) {
+    
+  }
   
 });
