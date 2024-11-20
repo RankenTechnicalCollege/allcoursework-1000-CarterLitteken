5ebede32ea5b69fca66b0985c2d987bebc1ce2ee
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-20 10:23:53 -0600

    hot

--- tests/JS_HOT3/todo_list.js
+++ tests/JS_HOT3/todo_list.js
@@ -4,19 +4,15 @@ const $ = selector => document.querySelector(selector);
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("click", todo)
+  $("#add").addEventListener("click", todo)
 
 });
 
 const todo = () => {
-  let key = event.key;
-  const message = document.getElementById("message")
+  const task = $("#task").value;
   const para = document.createElement("p")
-  const node = document.createTextNode("message");
-  if (key == "Enter") { 
-  	para.appendChild(node);
-    
-  }
+  const node = document.createTextNode(task);
+  para.appendChild(node);
   
 };
 
