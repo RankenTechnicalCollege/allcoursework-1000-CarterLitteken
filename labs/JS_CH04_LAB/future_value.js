12cc2a25c622c8fd35c26e0244cc0bada73c5102
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-21 11:05:55 -0600

    ch4lab

--- labs/JS_CH04_LAB/future_value.js
+++ labs/JS_CH04_LAB/future_value.js
@@ -2,18 +2,18 @@
 
 const $ = selector => document.querySelector(selector);
 
+const focusAndSelect = selector => {
+  const elem = $(selector);
+  elem.focus();
+  elem.select();
+};
+/*
 const calculateFV = (investment, rate, years) => {
   let futureValue = investment;
   for (let i = 1; i <= years; i++) {
     futureValue += futureValue * rate / 100;
   }
   return futureValue;
-};
-
-const focusAndSelect = selector => {
-  const elem = $(selector);
-  elem.focus();
-  elem.select();
 };
 
 const processEntries = () => {
@@ -41,23 +41,30 @@ const processEntries = () => {
     $("#futureValue").value = calculateFV(investment, rate, years).toFixed(2);
   }
 };
-
+*/
 document.addEventListener("DOMContentLoaded", () => {
-  $("#calculate").addEventListener("click", processEntries /*function () {
+  $("#calculate").addEventListener("click", (investment, rate, years) => {
+    let futureValue = investment;
+    for (let i = 1; i <= years; i++) {
+      futureValue += futureValue * rate / 100;
+    }
+    return futureValue;
+  });
+  $("#calculate").addEventListener("click", () => {
     const investment = parseFloat($("#investment").value);
     const rate = parseFloat($("#rate").value);
     const years = parseFloat($("#years").value);
     var error = null;
   
-    if (investment <= 0 || investment > 100000) {
+    if (isNaN(investment) || investment <= 0 || investment > 100000) {
       error = "Investment must be a number greater than 0 and less then or equal to 100,000.";
       focusAndSelect("#investment");
     }
-    else if (rate <= 0 || rate > 15) {
+    else if (isNaN(rate) || rate <= 0 || rate > 15) {
       error = "Interest rate must be a number greater than 0 and less then or equal to 15.";
       focusAndSelect("#rate");
     }
-    else if (years <= 0 || years > 50) {
+    else if (isNaN(years) || years <= 0 || years > 50) {
       error = "Years must be a number greater than 0 and less then or equal to 50.";
       focusAndSelect("#years");
     }
@@ -68,6 +75,8 @@ document.addEventListener("DOMContentLoaded", () => {
     else {
       $("#futureValue").value = calculateFV(investment, rate, years).toFixed(2);
     }
-  }()*/);
+  });
+
   $("#investment").focus();
+
 });
