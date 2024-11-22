1b2490517807ee0070b9173888b78d89727dbd47
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-22 09:16:24 -0600

    ch4lab

--- labs/JS_CH04_LAB/future_value.js
+++ labs/JS_CH04_LAB/future_value.js
@@ -7,57 +7,19 @@ const focusAndSelect = selector => {
   elem.focus();
   elem.select();
 };
-/*
-const calculateFV = (investment, rate, years) => {
-  let futureValue = investment;
-  for (let i = 1; i <= years; i++) {
-    futureValue += futureValue * rate / 100;
-  }
-  return futureValue;
-};
 
-const processEntries = () => {
-  const investment = parseFloat($("#investment").value);
-  const rate = parseFloat($("#rate").value);
-  const years = parseFloat($("#years").value);
-  var error = null;
-
-  if (isNaN(investment) || investment <= 0 || investment > 100000) {
-    error = "Investment must be greater than 0 and less then or equal to 100,000.";
-    focusAndSelect("#investment");
-  }
-  else if (isNaN(rate) || rate <= 0 || rate > 15) {
-    error = "Interest rate must be greater than 0 and less then or equal to 15.";
-    focusAndSelect("#rate");
-  }
-  else if (isNaN(years) || years <= 0 || years > 50) {
-    error = "Years must be greater than 0 and less then or equal to 50.";
-    focusAndSelect("#years");
-  }
-  if (error != null) {
-    alert(error.valueOf());
-  }
-  else {
-    $("#futureValue").value = calculateFV(investment, rate, years).toFixed(2);
-  }
-};
-*/
 document.addEventListener("DOMContentLoaded", () => {
   
-  $("#calculate").addEventListener("click", (investment, rate, years) => {
-    let futureValue = investment;
-    for (let i = 1; i <= years; i++) {
-      futureValue += futureValue * rate / 100;
-    }
-    return futureValue;
-  });
-
   $("#calculate").addEventListener("click", () => {
     const investment = parseFloat($("#investment").value);
     const rate = parseFloat($("#rate").value);
     const years = parseFloat($("#years").value);
     var error = null;
-  
+    let futureValue = investment;
+    for (let i = 1; i <= years; i++) {
+      futureValue += futureValue * rate / 100;
+    }
+
     if (isNaN(investment) || investment <= 0 || investment > 100000) {
       error = "Investment must be a number greater than 0 and less then or equal to 100,000.";
       focusAndSelect("#investment");
@@ -75,8 +37,9 @@ document.addEventListener("DOMContentLoaded", () => {
       alert(error.valueOf());
     }
     else {
-      $("#futureValue").value = calculateFV(investment, rate, years).toFixed(2);
+      $("#futureValue").value = futureValue.toFixed(2);
     }
+    
   });
 
   $("#investment").focus();
