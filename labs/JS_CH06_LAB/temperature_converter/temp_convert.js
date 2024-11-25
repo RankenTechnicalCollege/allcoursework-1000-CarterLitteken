a4744b3e9dc77ff8f14289eab4216118ce468109
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-11-25 11:18:48 -0600

    ch6lab

--- labs/JS_CH06_LAB/temperature_converter/temp_convert.js
+++ labs/JS_CH06_LAB/temperature_converter/temp_convert.js
@@ -5,8 +5,15 @@ const $ = selector => document.querySelector(selector);
 const toCelsius = $("#toCelsius");
 const toFahrenheit = $("#toFahrenheit");
 
+const enterDegrees = $("#enterDegrees");
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+
+
+    $("#toCelsius").addEventListener("change", () => {
+		if (toCelsius.checked == true){
+			enterDegrees.previousElementSibling.textContent = "Enter F degrees:";
+		}
+    });
 });
