c1d3795c6867776742b14b0e83587881e4b06cea
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-12-04 10:31:10 -0600

    ch6lab

--- labs/JS_CH06_LAB/test_scores/test_scores.js
+++ labs/JS_CH06_LAB/test_scores/test_scores.js
@@ -11,21 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
     let error2 = false;
 
     $("#add_to_arr").addEventListener("click", () => {
-        if (name != "") {
-            $("#name_error").value = "";
-            error1 = false;
-        } else {
+        if (name == "") {
             $("#name_error").value = "Please enter a name";
             error1 = true;
+        } else {
+            $("#name_error").value = "";
+            error1 = false;            
         }
 
-        if (score >= 0 && score <= 100) {
-            $("#score_error").value = "";
-            error2 = false;
-        } else {
+        if (isNaN(score) == true || score < 0 && score > 100) {
             $("#score_error").value = "Score must be between 0 and 100";
             error2 = true;
+        } else {
+            $("#score_error").value = "";
+            error2 = false;
         }
+
 
         if (error1 == false && error2 == false) {
             scores_arr.push(name,score);
