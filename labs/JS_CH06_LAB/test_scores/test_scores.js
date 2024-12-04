f16eec86e1868857ca18123270816f898e3ba2cf
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-12-04 08:54:31 -0600

    ch6lab

--- labs/JS_CH06_LAB/test_scores/test_scores.js
+++ labs/JS_CH06_LAB/test_scores/test_scores.js
@@ -3,5 +3,9 @@
 const $ = selector => document.querySelector(selector);
 
 const test_scores = () => {
-    
+    const scores_arr = [,]
+    let name = $("#name");
+    let score = $("#score");
+
+    if ()
 };
