36d99cc3dfaefc734df52f8d4939608d67457c6b
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-12-06 09:17:50 -0600

    ch6lab

--- labs/JS_CH06_LAB/test_scores/test_scores.js
+++ labs/JS_CH06_LAB/test_scores/test_scores.js
@@ -5,14 +5,14 @@ const $ = selector => document.querySelector(selector);
 document.addEventListener("DOMContentLoaded", () => {
 
     const scores_arr = [];
-    const name = $("#name");
-    const score = $("#score");
 
     $("#add_to_arr").addEventListener("click", () => {
+        const name = $("#name").value;
+        const score = parseFloat($("#score").value);
         let name_error = false;
         let score_error = false;
 
-        if (!isNaN(name.value) || name == "") {
+        if (!isNaN(name) || name == "") {   
             $("#name_error").value = "Please enter a name";
             name_error = true;
         } else {
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
             name_error = false;
         }
 
-        if (isNaN(score.value) || score < 0 && score > 100) {
+        if (isNaN(score) || score < 0 || score > 100) {
             $("#score_error").value = "Score must be between 0 and 100";
             score_error = true;
         } else {
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", () => {
         
         $("#name").focus();
     });
-
     //for scores_arr all names are even numbers and all scores are odd numbers
+    $("#display_results")
 
 });
