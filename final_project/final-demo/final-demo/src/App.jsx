1770813eb111d67909507fd67cd322f66ad0dd66
Carter_Liteken <Carter_Litteken@insideranken.org>
2024-12-10 08:29:31 -0600

    final-demo

--- final_project/final-demo/final-demo/src/App.jsx
+++ final_project/final-demo/final-demo/src/App.jsx
@@ -1,73 +1,103 @@
 import { useState } from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { nanoid } from 'nanoid';
+import AddStudent from './AddStudent';
 
 function App() {
   const [allStudents, setAllStudents] = useState(null);
 
   const students = [{
+    id: nanoid(),
     firstName: "Leeland",
     lastName: "Ebbing",
-    email: "lebbing0@patch.com"
+    email: "lebbing0@patch.com",
+    image: "student1.jpg"
   }, {
+    id: nanoid(),
     firstName: "Darb",
     lastName: "Scrogges",
-    email: "dscrogges1@tiny.cc"
+    email: "dscrogges1@tiny.cc",
+    image: "student2.jpg"
   }, {
+    id: nanoid(),
     firstName: "Rochella",
     lastName: "Trudgion",
-    email: "rtrudgion2@live.com"
+    email: "rtrudgion2@live.com",
+    image: "student3.jpg"
   }, {
+    id: nanoid(),
     firstName: "Tessie",
     lastName: "Hellyar",
-    email: "thellyar3@europa.eu"
+    email: "thellyar3@europa.eu",
+    image: "student4.jpg"
   }, {
+    id: nanoid(),
     firstName: "Sibeal",
     lastName: "Harrild",
-    email: "sharrild4@icio.us"
+    email: "sharrild4@icio.us",
+    image: "student5.jpg"
   }, {
+    id: nanoid(),
     firstName: "Richard",
     lastName: "Frensch",
-    email: "rfrensch5@nymag.com"
+    email: "rfrensch5@nymag.com",
+    image: "student6.jpg"
   }, {
+    id: nanoid(),
     firstName: "Beaufort",
     lastName: "Escudier",
-    email: "bescudier6@usnews.com"
+    email: "bescudier6@usnews.com",
+    image: "student7.jpg"
   }, {
+    id: nanoid(),
     firstName: "Kennedy",
     lastName: "Maxfield",
-    email: "kmaxfield7@squidoo.com"
+    email: "kmaxfield7@squidoo.com",
+    image: "student8.jpg"
   }, {
+    id: nanoid(),
     firstName: "Georgeanna",
     lastName: "O'Riordan",
-    email: "goriordan8@hhs.gov"
+    email: "goriordan8@hhs.gov",
+    image: "student9.jpg"
   }, {
+    id: nanoid(),
     firstName: "Marisa",
     lastName: "Carletto",
-    email: "mcarletto9@opensource.org"
-  }]
-  
+    email: "mcarletto9@opensource.org",
+    image: "student10.jpg"
+  }];
+
+  const saveStudents = (students) => {
+    setAllStudents(students);
+    
+  }
+
+  const addStudent = (newStudent) => {
+    const updatedStudents = [...allStudents, newStudent];
+    saveStudents(updatedStudents);
+  }
   
   return (
     <div className='container'>
       <div className='row'>
-        <div className='col-lg-2'>
+        {allStudents && allStudents.map((student) => (
+        <div className='col-lg-2' key={student.id}>
+          <div className='card'>
+          <img src={student.image} alt='Our Student' />
           <ul className='list-group list-group-flush'>
-            <li className='list-group-item'>One</li>
-            <li className='list-group-item'>Two</li>
-            <li className='list-group-item'>Three</li>
+            <li className='list-group-item'>{student.firstName}</li>
+            <li className='list-group-item'>{student.lastName}</li>
+            <li className='list-group-item'>{student.email}</li>
           </ul>
-        </div>
-        <div className='col-lg-2'>Col2</div>
-        <div className='col-lg-2'>Col3</div>
-        <div className='col-lg-2'>Col4</div>
-        <div className='col-lg-2'>Col5</div>
-        <div className='col-lg-2'>Col6</div>
-
-
+          </div>
+        </div>)
+  )}
       </div>
 
-
+        {!allStudents && <button type='button' className='btn btn-lg btn-success' onClick={() => setAllStudents(students)}>Save Student</button>}
+        <AddStudent addStudent={addStudent} />
     </div>
     
   )
